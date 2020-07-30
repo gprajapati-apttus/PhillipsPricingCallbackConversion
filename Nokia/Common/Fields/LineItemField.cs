f865660b16b2587739fa49ca65cacc090dacb8aa
gprajapati-apttus <63354552+gprajapati-apttus@users.noreply.github.com>
2020-07-30 14:05:22 +0530

    LS-3680: Merging line item fields in single class.

--- Nokia/Common/Fields/LineItemField.cs
+++ Nokia/Common/Fields/LineItemField.cs
@@ -1,6 +1,6 @@
 ﻿namespace Apttus.Lightsaber.Nokia.Common
 {
-    public class LineItemCustomField
+    public class LineItemField
     {
         public const string Custom_Product_Code__c = "Custom_Product_Code__c";
         public const string CustomProductValue__c = "CustomProductValue__c";
@@ -95,5 +95,45 @@
         public const string Sales_Margin__c = "Sales_Margin__c";
         public const string Item_Type_From_CAT__c = "Item_Type_From_CAT__c";
         public const string NokiaCPQ_IsArcadiaBundle__c = "NokiaCPQ_IsArcadiaBundle__c";
+
+        public const string Apttus_Config2__ConfigStatus__c = "Apttus_Config2__ConfigStatus__c";
+        public const string Apttus_Config2__IsReadOnly__c = "Apttus_Config2__IsReadOnly__c";
+        public const string Apttus_Config2__IsHidden__c = "Apttus_Config2__IsHidden__c";
+
+        public const string Apttus_Config2__ProductId__r_NokiaCPQ_Item_Type__c = "Apttus_Config2__ProductId__r.NokiaCPQ_Item_Type__c";
+        public const string Apttus_Config2__ProductId__r_Business_Group__c = "Apttus_Config2__ProductId__r.Business_Group__c";
+        public const string Apttus_Config2__ProductId__r_NokiaCPQ_Product_Discount_Category__c = "Apttus_Config2__ProductId__r.NokiaCPQ_Product_Discount_Category__c";
+        public const string Apttus_Config2__OptionId__r_NokiaCPQ_Product_Discount_Category__c = "Apttus_Config2__OptionId__r.NokiaCPQ_Product_Discount_Category__c";
+        public const string Apttus_Config2__ProductId__r_Apttus_Config2__ConfigurationType__c = "Apttus_Config2__ProductId__r.Apttus_Config2__ConfigurationType__c";
+        public const string Apttus_Config2__OptionId__r_Apttus_Config2__ConfigurationType__c = "Apttus_Config2__OptionId__r.Apttus_Config2__ConfigurationType__c";
+        public const string Apttus_Config2__ProductId__r_NokiaCPQ_Classification2__c = "Apttus_Config2__ProductId__r.NokiaCPQ_Classification2__c";
+        public const string Apttus_Config2__OptionId__r_NokiaCPQ_Classification2__c = "Apttus_Config2__OptionId__r.NokiaCPQ_Classification2__c";
+        public const string Apttus_Config2__OptionId__r_NokiaCPQ_Item_Type__c = "Apttus_Config2__OptionId__r.NokiaCPQ_Item_Type__c";
+        public const string Apttus_Config2__ProductId__r_NokiaCPQ_License_Usage__c = "Apttus_Config2__ProductId__r.NokiaCPQ_License_Usage__c";
+        public const string Apttus_Config2__OptionId__r_NokiaCPQ_License_Usage__c = "Apttus_Config2__OptionId__r.NokiaCPQ_License_Usage__c";
+        public const string Apttus_Config2__ProductId__r_Id = "Apttus_Config2__ProductId__r.Id";
+        public const string Apttus_Config2__OptionId__r_Id = "Apttus_Config2__OptionId__r.Id";
+        public const string Apttus_Config2__ProductId__r_ProductCode = "Apttus_Config2__ProductId__r.ProductCode";
+        public const string Apttus_Config2__OptionId__r_ProductCode = "Apttus_Config2__OptionId__r.ProductCode";
+        public const string Apttus_Config2__ProductId__r_Family = "Apttus_Config2__ProductId__r.Family";
+        public const string Apttus_Config2__ProductId__r_IsSSP__c = "Apttus_Config2__ProductId__r.IsSSP__c";
+        public const string Apttus_Config2__ProductId__r_Is_Dummy_Bundle_CPQ__c = "Apttus_Config2__ProductId__r.Is_Dummy_Bundle_CPQ__c";
+        public const string Apttus_Config2__OptionId__r_Is_Dummy_Bundle_CPQ__c = "Apttus_Config2__OptionId__r.Is_Dummy_Bundle_CPQ__c";
+        public const string Apttus_Config2__ProductId__r_Portfolio__c = "Apttus_Config2__ProductId__r.Portfolio__c";
+        public const string Apttus_Config2__OptionId__r_Number_of_GE_Ports__c = "Apttus_Config2__OptionId__r.Number_of_GE_Ports__c";
+        public const string Apttus_Config2__ProductId__r_Number_of_GE_Ports__c = "Apttus_Config2__ProductId__r.Number_of_GE_Ports__c";
+
+        public const string Apttus_Config2__OptionId__r_NokiaCPQ_Min_SM_North_America__c = "Apttus_Config2__OptionId__r.NokiaCPQ_Min_SM_North_America__c";
+        public const string Apttus_Config2__OptionId__r_NokiaCPQ_Min_SM_Latin_America__c = "Apttus_Config2__OptionId__r.NokiaCPQ_Min_SM_Latin_America__c";
+        public const string Apttus_Config2__OptionId__r_NokiaCPQMin_SM_Middle_East_and_Africa__c = "Apttus_Config2__OptionId__r.NokiaCPQMin_SM_Middle_East_and_Africa__c";
+        public const string Apttus_Config2__OptionId__r_NokiaCPQ_Min_SM_India__c = "Apttus_Config2__OptionId__r.NokiaCPQ_Min_SM_India__c";
+        public const string Apttus_Config2__OptionId__r_NokiaCPQ_Min_SM_Asia_Pacific_Japan__c = "Apttus_Config2__OptionId__r.NokiaCPQ_Min_SM_Asia_Pacific_Japan__c";
+        public const string Apttus_Config2__OptionId__r_NokiaCPQ_Min_SM_Greater_China__c = "Apttus_Config2__OptionId__r.NokiaCPQ_Min_SM_Greater_China__c";
+        public const string Apttus_Config2__OptionId__r_NokiaCPQ_Min_SM_Europe__c = "Apttus_Config2__OptionId__r.NokiaCPQ_Min_SM_Europe__c";
+
+        public const string Apttus_Config2__ProductId__r_NokiaCPQ_Maximum_IRP_Discount__c = "Apttus_Config2__ProductId__r.NokiaCPQ_Maximum_IRP_Discount__c";
+        public const string Apttus_Config2__OptionId__r_NokiaCPQ_Maximum_IRP_Discount__c = "Apttus_Config2__OptionId__r.NokiaCPQ_Maximum_IRP_Discount__c";
+        public const string Apttus_Config2__ProductId__r_NokiaCPQ_OEM__c = "Apttus_Config2__ProductId__r.NokiaCPQ_OEM__c";
+        public const string Apttus_Config2__OptionId__r_NokiaCPQ_OEM__c = "Apttus_Config2__OptionId__r.NokiaCPQ_OEM__c";
     }
 }
